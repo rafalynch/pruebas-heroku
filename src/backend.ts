503da9f88279435d95bf07a8445cedab8cab4ddc
Rafa Lynch <rafa_lynch@hotmail.com>
2021-09-05 23:55:38 -0300

    prueba get

--- src/backend.ts
+++ src/backend.ts
@@ -7,7 +7,7 @@ import * as token from "rand-token";
 import { state } from "./state";
 
 const app = express();
-const PORT = "4000";
+const PORT = process.env.PORT || "4000";
 
 app.use(express.json());
 app.use(cors());
